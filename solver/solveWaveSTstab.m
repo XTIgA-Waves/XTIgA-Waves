a7cde07464048007b5d590485ea2fae9293800e4
gabrielel93 <gabriele.loli01@universitadipavia.it>
2024-06-07 12:18:19 +0200

    Upgraded solvers

--- solver/solveWaveSTstab.m
+++ solver/solveWaveSTstab.m
@@ -11,7 +11,9 @@ p = inputParser;
 errorMsg = 'In presence of Robin boundary condition, the ''efficient'' version of the linear solver is not exact.';
 validationFcn = @(x) assert((isempty(rbnSides)||~strcmp(x,'dirEff')),errorMsg);
 addParameter(p,'Solver','dir',validationFcn);
+addParameter(p,'ExactSol',false,@islogical);
 addParameter(p,'Error',false,@islogical);
+addParameter(p,'ErrorFine',false,@islogical);
 addParameter(p,'Energy',false,@islogical);
 p.parse(varargin{:});
 cellfun(@(x) assignin('caller',x,p.Results.(x)), fieldnames(p.Results));
@@ -21,18 +23,14 @@ clear p varargin;
 if ~exist('geometryS','var')
     geometryS = geo_load(geoName);
 end
-clear geoName;
 dimS = geometryS.rdim;
 nsubS = ceil((nsubS./(geometryS.nurbs.number-geometryS.nurbs.order+1))-1); %#ok<*NODEF>
 [rknotsS,zetaS] = kntrefine(geometryS.nurbs.knots,nsubS,degreeS,regularityS);
-clear nsubS;
 
-nurbsT = nrbline([0,0],[T,0]);
-geometryT = geo_load(nurbsT);
-clear nurbsT;
+geometryT = geo_load(nrbline([0,0],[T,0]));
 nsubT = ceil((nsubT./(geometryT.nurbs.number-geometryT.nurbs.order+1))-1);
 [rknotsT,zetaT] = kntrefine(geometryT.nurbs.knots,nsubT,degreeT,regularityT);
-clear nsubT;
+clear nsubS nsubT;
 
 %% Check for periodic conditions, and consistency with other boundary conditions
 if ~isempty(perDir)
@@ -49,17 +47,14 @@ nquad = degreeS+1;
 quadRule = msh_gauss_nodes(nquad);
 [qn,qw] = msh_set_quad_nodes(zetaS,quadRule);
 mshS = msh_cartesian(zetaS,qn,qw,geometryS);
-clear qn qw;
 solution.hs = max(msh_precompute(mshS).element_size);
 spaceS = sp_bspline(rknotsS,degreeS,mshS,[],perDir);
 
 nquad = degreeT+1;
 quadRule = msh_gauss_nodes(nquad);
-clear nquad;
 [qn,qw] = msh_set_quad_nodes(zetaT,quadRule);
-clear quadRule;
 mshT = msh_cartesian(zetaT,qn,qw,geometryT);
-clear geometryT qn qw;
+clear geometryT nquad quadRule qn qw;
 solution.ht = max(msh_precompute(mshT).element_size);
 spaceT = sp_bspline(rknotsT,degreeT,mshT);
 
@@ -78,14 +73,14 @@ Ms = op_u_v_tp(spaceS,spaceS,mshS);
 Mt = op_u_v_tp(spaceT,spaceT,mshT);
 Kt = op_gradu_gradv_tp(spaceT,spaceT,mshT);
 
-if~isempty(rbnSides)
-    Mt = op_vel_dot_gradu_v_tp(spaceT,spaceT,mshT,@(t) ones(size(t)));
+if ~isempty(rbnSides)
+    Wt = op_vel_dot_gradu_v_tp(spaceT,spaceT,mshT,@(t) ones(size(t)));
     nnzMr = sum(arrayfun(@(ibnd) numel(spaceS.boundary(ibnd).dofs),rbnSides))*(2*max(degreeS)+1);
     Mr = spalloc(size(Ms,1),size(Ms,2),nnzMr);
     for iside = rbnSides
         dofs = spaceS.boundary(iside).dofs;
         if mshS.ndim == 1
-            Mr(dofs,dofs) = theta*c(mshS.map(iside-1));
+            Mr(dofs,dofs) = theta*c(mshS.map(iside-1),0);
         else
             mshSide = msh_eval_boundary_side (mshS,iside);
             spSide = sp_precompute (spaceS.boundary(iside), mshSide,'value',true,'gradient',false);
@@ -114,41 +109,49 @@ clear mshT spaceT;
 
 %% Assembling Global structures
 solution.geometryST = geo_load_st(geometryS,0,T);
-clear geometryS;
 nquad = [degreeS,degreeT]+1;
 quadRule = msh_gauss_nodes(nquad);
-clear nquad;
 [qn,qw] = msh_set_quad_nodes([zetaS(:)',{zetaT}],quadRule);
-clear quadRule;
 solution.mshST = msh_cartesian([zetaS(:)',{zetaT}],qn,qw,solution.geometryST);
-clear qn qw zetaS zetaT;
 solution.spaceST = sp_bspline([rknotsS(:)',{rknotsT}],[degreeS,degreeT],solution.mshST,[],perDir);
-clear rknotsT;
+clear geometryS quadRule qn qw zetaS zetaT rknotsT;
 rhs = op_f_v_tp(solution.spaceST,solution.mshST,f);
 solution.u = zeros (solution.spaceST.ndof,1);
 
 %% Apply Dirichlet boundary conditions and initial condition (u(0)=u_0)
 switch dimS
     case 1
-        gDrchlt = @(x,t,iside) InitDrchlt1d(gDrchlt,gInit,x,t,dimS,iside);
+        if ExactSol
+            gDrchlt = @(x,t,iside) u_ex(x,t);
+        else
+            gDrchlt = @(x,t,iside) InitDrchlt1d(gDrchlt,gInit,x,t,dimS,iside);
+        end
     case 2
-        gDrchlt = @(x,y,t,iside) InitDrchlt2d(gDrchlt,gInit,x,y,t,dimS,iside);
+        if ExactSol
+            gDrchlt = @(x,y,t,iside) u_ex(x,y,t);
+        else
+            gDrchlt = @(x,y,t,iside) InitDrchlt2d(gDrchlt,gInit,x,y,t,dimS,iside);
+        end
     case 3
-        gDrchlt = @(x,y,z,t,iside) InitDrchlt3d(gDrchlt,gInit,x,y,z,t,dimS,iside);
+        if ExactSol
+            gDrchlt = @(x,y,z,t,iside) u_ex(x,y,z,t);
+        else
+            gDrchlt = @(x,y,z,t,iside) InitDrchlt3d(gDrchlt,gInit,x,y,z,t,dimS,iside);
+        end
 end
-[uForced, trialForcedDofs] = sp_drchlt_l2_proj (solution.spaceST, solution.mshST, gDrchlt, union(drchltSides,numel(solution.spaceST.boundary)-1));
+[uForced, trialForcedDofs] = sp_drchlt_l2_proj(solution.spaceST,solution.mshST,gDrchlt,union(drchltSides,2*dimS+1));
 solution.u(trialForcedDofs) = uForced;
 clear uForced;
 
-testforcedDofs = [];
+testForcedDofs = [];
 nent = 0;
 for iside = union(drchltSides,numel(solution.spaceST.boundary))
     nent = nent + solution.mshST.boundary(iside).nel * solution.spaceST.boundary(iside).nsh_max^2;
-    testforcedDofs = union (testforcedDofs, solution.spaceST.boundary(iside).dofs);
+    testForcedDofs = union (testForcedDofs, solution.spaceST.boundary(iside).dofs);
 end
 
 trialIntDofs = setdiff (1:solution.spaceST.ndof, trialForcedDofs);
-testForcedDofs = setdiff (1:solution.spaceST.ndof, testforcedDofs);
+testIntDofs = setdiff (1:solution.spaceST.ndof, testForcedDofs);
 
 drchltDofsST = [];
 drchltDofsS = [];
@@ -162,32 +165,31 @@ solution.nDofS = numel(intDofsS);
 clear nent spaceS;
 
 tmp0 = Ms*solution.u(solution.spaceST.boundary(end-1).dofs)*Kt(:,1)'...
-      -Ks*solution.u(solution.spaceST.boundary(end-1).dofs)*Mt(:,1)'...
-      +Ks*solution.u(solution.spaceST.boundary(end-1).dofs)*Pt(:,1)';
+    -Ks*solution.u(solution.spaceST.boundary(end-1).dofs)*Mt(:,1)'...
+    +Ks*solution.u(solution.spaceST.boundary(end-1).dofs)*Pt(:,1)';
 
 tmpDrchlt = Ms(:,drchltDofsS)*reshape(solution.u(drchltDofsST),numel(drchltDofsS),size(Kt,2)-1)*Kt(:,2:end)'...
-           -Ks(:,drchltDofsS)*reshape(solution.u(drchltDofsST),numel(drchltDofsS),size(Kt,2)-1)*Mt(:,2:end)'...
-           +Ks(:,drchltDofsS)*reshape(solution.u(drchltDofsST),numel(drchltDofsS),size(Kt,2)-1)*Pt(:,2:end)';
+    -Ks(:,drchltDofsS)*reshape(solution.u(drchltDofsST),numel(drchltDofsS),size(Kt,2)-1)*Mt(:,2:end)'...
+    +Ks(:,drchltDofsS)*reshape(solution.u(drchltDofsST),numel(drchltDofsS),size(Kt,2)-1)*Pt(:,2:end)';
 
 if ~isempty(rbnSides)
-    tmp0 = tmp0 -Mr*solution.u(solution.spaceST.boundary(end-1).dofs)*Mt(:,1)';
-    tmpDrchlt = tmpDrchlt -Mr(:,drchltDofsS)*reshape(solution.u(drchltDofsST),numel(drchltDofsS),size(Kt,2)-1)*Mt(:,2:end)';
+    tmp0 = tmp0 -Mr*solution.u(solution.spaceST.boundary(end-1).dofs)*Wt(:,1)';
+    tmpDrchlt = tmpDrchlt -Mr(:,drchltDofsS)*reshape(solution.u(drchltDofsST),numel(drchltDofsS),size(Kt,2)-1)*Wt(:,2:end)';
 end
 
-rhs(testForcedDofs) = rhs(testForcedDofs) + tmp0(testForcedDofs(:)) + tmpDrchlt(testForcedDofs(:));
-clear trialForcedDofs testforcedDofs tmp0 tmpDrchlt;
+rhs(testIntDofs) = rhs(testIntDofs) + tmp0(testIntDofs(:)) + tmpDrchlt(testIntDofs(:));
+clear trialForcedDofs testForcedDofs tmp0 tmpDrchlt drchltDofsS drchltDofsST;
 
 %% Apply Initial conditions (u'(0)=u_1)
 dofs = solution.spaceST.boundary(2*dimS+1).dofs;
 mshSide = msh_eval_boundary_side (solution.mshST,2*dimS+1);
-clear dimS;
 spSide = sp_eval_boundary_side (solution.spaceST,mshSide);
 x = cell(solution.mshST.rdim,1);
 for idim = 1:solution.mshST.rdim
     x{idim} = reshape (mshSide.geo_map(idim,:,:),mshSide.nqn,mshSide.nel);
 end
 rhs(dofs) = rhs(dofs) + op_f_v(spSide,mshSide,gInitDer(x{:}));
-clear spSide dofs;
+clear mshSide spSide x dofs;
 
 %% Apply Neumann boundary conditions
 for iside = nmnnSides
@@ -198,9 +200,13 @@ for iside = nmnnSides
     for idim = 1:solution.mshST.rdim
         x{idim} = reshape (mshSide.geo_map(idim,:,:),mshSide.nqn,mshSide.nel);
     end
-    rhs(dofs) = rhs(dofs) + op_f_v(spSide,mshSide,gNmnn(x{:}));
+    if ExactSol
+        rhs(dofs) = rhs(dofs) + op_fdotn_v(spSide,mshSide,grad_u_ex_c2(x{:}));
+    else
+        rhs(dofs) = rhs(dofs) + op_f_v(spSide,mshSide,gNmnn(x{:}));
+    end
 end
-clear mshSide spSide x dofs;
+clear spSide mshSide x dofs;
 
 %% Apply Robin boundary conditions
 for iside = rbnSides
@@ -209,9 +215,14 @@ for iside = rbnSides
     spSide = sp_eval_boundary_side (solution.spaceST,mshSide);
     x = cell(solution.mshST.rdim,1);
     for idim = 1:solution.mshST.rdim
-        x{idim} = reshape (mshSide.geo_map(idim,:,:),mshSide.nqn,mshSide.nel);
+        x{idim} = reshape(mshSide.geo_map(idim,:,:),mshSide.nqn,mshSide.nel);
     end
-    rhs(dofs) = rhs(dofs) + op_f_v(spSide,mshSide,gRbn(x{:}));
+    if ExactSol
+        rhs(dofs) = rhs(dofs) + op_fdotn_v(spSide,mshSide,grad_u_ex_c2(x{:}))...
+            + op_f_v(spSide,mshSide,theta*c_dt_u_ex(x{:}));
+    else
+        rhs(dofs) = rhs(dofs) + op_f_v(spSide,mshSide,gRbn(x{:}));
+    end
 end
 clear mshSide spSide x dofs;
 
@@ -223,15 +234,15 @@ clear mshSide spSide x dofs;
 % case 'precT':   GMRES solver preconditioned by dirEff.
 switch Solver
     case 'dir'
-        Mat=kron(Mt(1:end-1,2:end)-Pt(1:end-1,2:end),Ks(intDofsS,intDofsS))-kron(Kt(1:end-1,2:end),Ms(intDofsS,intDofsS));
-        if~isempty(rbnSides)
-            Mat=Mat+kron(Wt(1:end-1,2:end),Mr(intDofsS,intDofsS));
+        Mat = kron(Mt(1:end-1,2:end)-Pt(1:end-1,2:end),Ks(intDofsS,intDofsS))-kron(Kt(1:end-1,2:end),Ms(intDofsS,intDofsS));
+        if ~isempty(rbnSides)
+            Mat = Mat+kron(Wt(1:end-1,2:end),Mr(intDofsS,intDofsS));
         end
-        solution.u(trialIntDofs) = Mat\rhs(intDofs2);
+        solution.u(trialIntDofs) = Mat\rhs(testIntDofs);
     case 'dirEff'
         [U,Ds,Q,Z,Tr,A] = FTparameters(Ks(intDofsS,intDofsS),Kt(1:end-1,2:end),Ms(intDofsS,intDofsS),Mt(1:end-1,2:end)-Pt(1:end-1,2:end));
         clear Ks Kt Ms Mt intDofsS;
-        solution.u(trialIntDofs) = FTapplication(rhs(intDofs2),U,Ds,Q,Z,Tr,A);
+        solution.u(trialIntDofs) = FTapplication(rhs(testIntDofs),U,Ds,Q,Z,Tr,A);
         clear U Ds Q Z Tr A;
     case 'precT'
         [U,Ds,Q,Z,Tr,A] = FTparameters(Ks(intDofsS,intDofsS),Kt(1:end-1,2:end),Ms(intDofsS,intDofsS),Mt(1:end-1,2:end)-Pt(1:end-1,2:end));
@@ -244,28 +255,27 @@ switch Solver
         if~exist('tol','var')
             tol=10^-12;
         end
-        [y,~,relres,iter] = gmres(Mat,rhs(intDofs2),50,tol,numel(intDofs1),Prec);
+        [y,~,relres,iter] = gmres(Mat,rhs(testIntDofs),50,tol,numel(trialIntDofs),Prec);
         solution.u(trialIntDofs) = y;
         fprintf(1,'\n*Solver info: iter=[%d,%d]; Relres=%g*\n',iter(1),iter(2),relres);
-        clear intDofs2 U Ds Q Z Tr A Ks Kt Ms Mt Mr Mt intDofsS iter relres;
+        clear testIntDofs U Ds Q Z Tr A Ks Kt Ms Mt Mr Wt intDofsS iter relres;
 end
-solution.nDof = numel(intDofs1);
-clear intDofs1 intDofs2 rhs;
+solution.nDof = numel(trialIntDofs);
+clear trialIntDofs rhs;
 
-%% Compute Errors
+%% Errori in norma L2 e in norma H1
 if Error
-    [errH1,errL2,errH1s] = sp_h1_c_error_tp (solution.spaceST, solution.mshST, solution.u, u_ex, grad_u_ex, c);
-    [normH1,normL2,normH1s] = sp_h1_c_error_tp (solution.spaceST, solution.mshST, zeros(size(solution.u)), u_ex, grad_u_ex, c);
+    [errH1,errL2,errH1s] = sp_h1_c_error_tp(solution.spaceST,solution.mshST,solution.u,u_ex,grad_u_ex,c);
+    [normH1,normL2,normH1s] = sp_h1_c_error_tp(solution.spaceST,solution.mshST,zeros(size(solution.u)),u_ex, grad_u_ex,c);
     solution.relErrH1 = errH1/normH1;
     solution.relErrL2 = errL2/normL2;
     solution.relErrH1s = errH1s/normH1s;
-    clear errH1s errH1 errL2 normH1s normH1 normL2;
 end
 
 if ErrorFine
     solFine=load(solFine).solution;
-    [errH1, errL2, errH1s] = sp_h1_error_spline_ex (solution.spaceST, solution.mshST, solution.u, solFine.spaceST, solution.geometryST, solFine.u,c);
-    [normH1,normL2,normH1s] = sp_h1_error_spline_ex (solution.spaceST, solution.mshST, solution.u*0, solFine.spaceST, solution.geometryST, solFine.u,c);
+    [errH1, errL2, errH1s] = sp_h1_error_spline_ex(solution.spaceST,solution.mshST,solution.u,solFine.spaceST,solution.geometryST,solFine.u,c);
+    [normH1,normL2,normH1s] = sp_h1_error_spline_ex(solution.spaceST,solution.mshST,solution.u*0,solFine.spaceST,solution.geometryST,solFine.u,c);
     solution.relErrH1=errH1/normH1;
     solution.relErrL2=errL2/normL2;
     solution.relErrH1s=errH1s/normH1s;
@@ -275,7 +285,9 @@ end
 if Energy
     solution.E0Et = computeEnergy(solution.mshST.qn,solution.mshST.qw,solution.u,c,solution.spaceST,solution.geometryST,solution.mshST.map_der,[0,1]);
 end
+
 end
+
 
 function gDrchlt = InitDrchlt3d(gDrchlt,gInit,x,y,z,t,dimS,iside)
 if iside == 2*dimS+1
--- solver/solveWaveSTstabZank.m
+++ solver/solveWaveSTstabZank.m
@@ -1,9 +1,9 @@
-function solution = solveWaveSTstabZankP(problemData,methodData,varargin)
+function solution = solveWaveSTstabZank(problemData,methodData,varargin)
 
 %% Unwrapping input structures
-cellfun(@(x) assignin('caller',x,problemData.(x)), fieldnames(problemData));
+cellfun(@(x) assignin('caller',x,problemData.(x)),fieldnames(problemData));
 clear problemData;
-cellfun(@(x) assignin('caller',x,methodData.(x)), fieldnames(methodData));
+cellfun(@(x) assignin('caller',x,methodData.(x)),fieldnames(methodData));
 clear methodData;
 
 %% Chosing linear solver
@@ -11,7 +11,9 @@ p = inputParser;
 errorMsg = 'In presence of Robin boundary condition, the ''efficient'' version of the linear solver is not exact.';
 validationFcn = @(x) assert((isempty(rbnSides)||~strcmp(x,'dirEff')),errorMsg);
 addParameter(p,'Solver','dir',validationFcn);
+addParameter(p,'ExactSol',false,@islogical);
 addParameter(p,'Error',false,@islogical);
+addParameter(p,'ErrorFine',false,@islogical);
 addParameter(p,'Energy',false,@islogical);
 p.parse(varargin{:});
 cellfun(@(x) assignin('caller',x,p.Results.(x)), fieldnames(p.Results));
@@ -21,19 +23,15 @@ clear p varargin;
 if ~exist('geometryS','var')
     geometryS = geo_load(geoName);
 end
-clear geoName;
 dimS = geometryS.rdim;
 nsubS = ceil((nsubS./(geometryS.nurbs.number-geometryS.nurbs.order+1))-1); %#ok<*NODEF>
 [rknotsS,zetaS] = kntrefine(geometryS.nurbs.knots,nsubS,degreeS,regularityS);
-clear nsubS;
 
-nurbsT = nrbline([0,0],[T,0]);
-geometryT = geo_load(nurbsT);
-clear nurbsT;
+geometryT = geo_load(nrbline([0,0],[T,0])) ;
 nsubT = ceil((nsubT./(geometryT.nurbs.number-geometryT.nurbs.order+1))-1);
 [rknotsT,zetaT] = kntrefine(geometryT.nurbs.knots,nsubT,degreeT,regularityT);
 [rknotsTpm1,~] = kntrefine(geometryT.nurbs.knots,nsubT,degreeT-1,-1);
-clear nsubT;
+clear nsubS nsubT;
 
 %% Check for periodic conditions, and consistency with other boundary conditions
 if ~isempty(perDir)
@@ -56,11 +54,9 @@ spaceS = sp_bspline(rknotsS,degreeS,mshS,[],perDir);
 
 nquad = degreeT+1;
 quadRule = msh_gauss_nodes(nquad);
-clear nquad;
 [qn,qw] = msh_set_quad_nodes(zetaT,quadRule);
-clear quadRule;
 mshT = msh_cartesian(zetaT,qn,qw,geometryT);
-clear geometryT qn qw;
+clear nquad quadRule geometryT qn qw;
 solution.ht = max(msh_precompute(mshT).element_size);
 spaceT = sp_bspline(rknotsT,degreeT,mshT);
 spaceTpm1 = sp_bspline(rknotsTpm1,degreeT-1,mshT);
@@ -82,14 +78,14 @@ Gt=op_u_v_tp(spaceTpm1,spaceT,mshT);
 Mtt=Gt*(Mt\Gt');
 Kt = op_gradu_gradv_tp(spaceT,spaceT,mshT);
 
-if~isempty(rbnSides)
+if ~isempty(rbnSides)
     Wt = op_vel_dot_gradu_v_tp(spaceT,spaceT,mshT,@(t) ones(size(t)));
     nnzMr = sum(arrayfun(@(ibnd) numel(spaceS.boundary(ibnd).dofs),rbnSides))*(2*max(degreeS)+1);
     Mr = spalloc(size(Ms,1),size(Ms,2),nnzMr);
     for iside = rbnSides
         dofs = spaceS.boundary(iside).dofs;
         if mshS.ndim == 1
-            Mr(dofs,dofs) = theta*c(mshS.map(iside-1));
+            Mr(dofs,dofs) = theta*c(mshS.map(iside-1),0);
         else
             mshSide = msh_eval_boundary_side (mshS,iside);
             spSide = sp_precompute (spaceS.boundary(iside), mshSide,'value',true,'gradient',false);
@@ -101,45 +97,53 @@ if~isempty(rbnSides)
         end
     end
 end
-clear mshS mshSide spSide nnzMr dofs mshT spaceT;
+clear mshS mshSide spSide nnzMr mshT spaceT;
 
 %% Assembling Global structures
 solution.geometryST = geo_load_st(geometryS,0,T);
-clear geometryS;
 nquad = [degreeS,degreeT]+1;
 quadRule = msh_gauss_nodes(nquad);
-clear nquad;
 [qn,qw] = msh_set_quad_nodes([zetaS(:)',{zetaT}],quadRule);
-clear quadRule;
 solution.mshST = msh_cartesian([zetaS(:)',{zetaT}],qn,qw,solution.geometryST);
-clear qn qw zetaS zetaT;
 solution.spaceST = sp_bspline([rknotsS(:)',{rknotsT}],[degreeS,degreeT],solution.mshST,[],perDir);
-clear rknotsT;
+clear geometryS qn qw quadRule zetaS zetaT rknotsT;
 rhs = op_f_v_tp(solution.spaceST,solution.mshST,f);
-solution.u = zeros (solution.spaceST.ndof,1);
+solution.u = zeros(solution.spaceST.ndof,1);
 
 %% Apply Dirichlet boundary conditions and initial condition (u(0)=u_0)
 switch dimS
     case 1
-        gDrchlt = @(x,t,iside) InitDrchlt1d(gDrchlt,gInit,x,t,dimS,iside);
+        if ExactSol
+            gDrchlt = @(x,t,iside) u_ex(x,t);
+        else
+            gDrchlt = @(x,t,iside) InitDrchlt1d(gDrchlt,gInit,x,t,dimS,iside);
+        end
     case 2
-        gDrchlt = @(x,y,t,iside) InitDrchlt2d(gDrchlt,gInit,x,y,t,dimS,iside);
+        if ExactSol
+            gDrchlt = @(x,y,t,iside) u_ex(x,y,t);
+        else
+            gDrchlt = @(x,y,t,iside) InitDrchlt2d(gDrchlt,gInit,x,y,t,dimS,iside);
+        end
     case 3
-        gDrchlt = @(x,y,z,t,iside) InitDrchlt3d(gDrchlt,gInit,x,y,z,t,dimS,iside);
+        if ExactSol
+            gDrchlt = @(x,y,z,t,iside) u_ex(x,y,z,t);
+        else
+            gDrchlt = @(x,y,z,t,iside) InitDrchlt3d(gDrchlt,gInit,x,y,z,t,dimS,iside);
+        end
 end
-[uForced, trialForcedDofs] = sp_drchlt_l2_proj (solution.spaceST, solution.mshST, gDrchlt, union(drchltSides,numel(solution.spaceST.boundary)-1));
+[uForced, trialForcedDofs] = sp_drchlt_l2_proj(solution.spaceST,solution.mshST,gDrchlt,union(drchltSides,2*dimS+1));
 solution.u(trialForcedDofs) = uForced;
 clear uForced;
 
 testforcedDofs = [];
 nent = 0;
 for iside = union(drchltSides,numel(solution.spaceST.boundary))
-    nent = nent + solution.mshST.boundary(iside).nel * solution.spaceST.boundary(iside).nsh_max^2;
-    testforcedDofs = union (testforcedDofs, solution.spaceST.boundary(iside).dofs);
+    nent = nent + solution.mshST.boundary(iside).nel*solution.spaceST.boundary(iside).nsh_max^2;
+    testforcedDofs = union(testforcedDofs,solution.spaceST.boundary(iside).dofs);
 end
 
-trialIntDofs = setdiff (1:solution.spaceST.ndof, trialForcedDofs);
-testForcedDofs = setdiff (1:solution.spaceST.ndof, testforcedDofs);
+trialIntDofs = setdiff(1:solution.spaceST.ndof,trialForcedDofs);
+testIntDofs = setdiff(1:solution.spaceST.ndof,testforcedDofs);
 
 drchltDofsST = [];
 drchltDofsS = [];
@@ -153,54 +157,60 @@ solution.nDofS = numel(intDofsS);
 clear nent spaceS;
 
 tmp0 = Ms*solution.u(solution.spaceST.boundary(end-1).dofs)*Kt(:,1)'...
-      -Ks*solution.u(solution.spaceST.boundary(end-1).dofs)*Mt(:,1)'...
-      +Ks*solution.u(solution.spaceST.boundary(end-1).dofs)*Pt(:,1)';
+    - Ks*solution.u(solution.spaceST.boundary(end-1).dofs)*Mtt(:,1)';
 
 tmpDrchlt = Ms(:,drchltDofsS)*reshape(solution.u(drchltDofsST),numel(drchltDofsS),size(Kt,2)-1)*Kt(:,2:end)'...
-           -Ks(:,drchltDofsS)*reshape(solution.u(drchltDofsST),numel(drchltDofsS),size(Kt,2)-1)*Mt(:,2:end)'...
-           +Ks(:,drchltDofsS)*reshape(solution.u(drchltDofsST),numel(drchltDofsS),size(Kt,2)-1)*Pt(:,2:end)';
+    - Ks(:,drchltDofsS)*reshape(solution.u(drchltDofsST),numel(drchltDofsS),size(Kt,2)-1)*Mtt(:,2:end)';
 
 if ~isempty(rbnSides)
-    tmp0 = tmp0 -Mr*solution.u(solution.spaceST.boundary(end-1).dofs)*Wt(:,1)';
-    tmpDrchlt = tmpDrchlt -Mr(:,drchltDofsS)*reshape(solution.u(drchltDofsST),numel(drchltDofsS),size(Kt,2)-1)*Wt(:,2:end)';
+    tmp0 = tmp0 - Mr*solution.u(solution.spaceST.boundary(end-1).dofs)*Wt(:,1)';
+    tmpDrchlt = tmpDrchlt - Mr(:,drchltDofsS)*reshape(solution.u(drchltDofsST),numel(drchltDofsS),size(Kt,2)-1)*Wt(:,2:end)';
 end
 
-rhs(testForcedDofs) = rhs(testForcedDofs) + tmp0(testForcedDofs(:)) + tmpDrchlt(testForcedDofs(:));
+rhs(testIntDofs) = rhs(testIntDofs) + tmp0(testIntDofs(:)) + tmpDrchlt(testIntDofs(:));
 clear trialForcedDofs testforcedDofs tmp0 tmpDrchlt;
 
 %% Apply Initial conditions (u'(0)=u_1)
 dofs = solution.spaceST.boundary(2*dimS+1).dofs;
 mshSide = msh_eval_boundary_side (solution.mshST,2*dimS+1);
-clear dimS;
 spSide = sp_eval_boundary_side (solution.spaceST,mshSide);
 x = cell(solution.mshST.rdim,1);
 for idim = 1:solution.mshST.rdim
     x{idim} = reshape (mshSide.geo_map(idim,:,:),mshSide.nqn,mshSide.nel);
 end
-rhs(dofs) = rhs(dofs) + op_f_v (spSide,mshSide,gInitDer(x{:}));
-clear spSide dofs;
+rhs(dofs) = rhs(dofs) + op_f_v(spSide,mshSide,gInitDer(x{:}));
+clear mshSide x dofs;
 
 %% Apply Neumann boundary conditions
 for iside = nmnnSides
     dofs = solution.spaceST.boundary(iside).dofs;
-    mshSide = msh_eval_boundary_side (solution.mshST,iside);
-    spSide = sp_eval_boundary_side (solution.spaceST,mshSide);
+    mshSide = msh_eval_boundary_side(solution.mshST,iside);
+    spSide = sp_eval_boundary_side(solution.spaceST,mshSide);
     for idim = 1:solution.mshST.rdim
-        x{idim} = reshape (mshSide.geo_map(idim,:,:),mshSide.nqn,mshSide.nel);
+        x{idim} = reshape(mshSide.geo_map(idim,:,:),mshSide.nqn,mshSide.nel);
     end
-    rhs(dofs) = rhs(dofs) + op_f_v (spSide,mshSide,gNmnn(x{:}));
+    if ExactSol
+        rhs(dofs) = rhs(dofs) + op_fdotn_v(spSide,mshSide,grad_u_ex_c2(x{:}));
+    else
+        rhs(dofs) = rhs(dofs) + op_f_v(spSide,mshSide,gNmnn(x{:}));
+    end
 end
-clear mshSide spSide x dofs;
+clear spSide mshSide x dofs;
 
 %% Apply Robin boundary conditions
 for iside = rbnSides
     dofs = solution.spaceST.boundary(iside).dofs;
-    mshSide = msh_eval_boundary_side (solution.mshST,iside);
-    spSide = sp_eval_boundary_side (solution.spaceST,mshSide);
+    mshSide = msh_eval_boundary_side(solution.mshST,iside);
+    spSide = sp_eval_boundary_side(solution.spaceST,mshSide);
     for idim = 1:solution.mshST.rdim
-        x{idim} = reshape (mshSide.geo_map(idim,:,:),mshSide.nqn,mshSide.nel);
+        x{idim} = reshape(mshSide.geo_map(idim,:,:),mshSide.nqn,mshSide.nel);
     end
-    rhs(dofs) = rhs(dofs) + op_f_v (spSide,mshSide,gRbn(x{:}));
+    if ExactSol
+        rhs(dofs) = rhs(dofs) + op_fdotn_v(spSide,mshSide,grad_u_ex_c2(x{:}))...
+            + op_f_v(spSide,mshSide,theta*c_dt_u_ex(x{:}));
+    else
+        rhs(dofs) = rhs(dofs) + op_f_v(spSide,mshSide,gRbn(x{:}));
+    end
 end
 clear mshSide spSide x dofs;
 
@@ -212,15 +222,15 @@ clear mshSide spSide x dofs;
 % case 'precT':   GMRES solver preconditioned by dirEff.
 switch Solver
     case 'dir'
-        Mat=kron(Mtt(1:end-1,2:end),Ks(intDofsS,intDofsS))-kron(Kt(1:end-1,2:end),Ms(intDofsS,intDofsS));
-        if~isempty(rbnSides)
-            Mat=Mat+kron(Wt(1:end-1,2:end),Mr(intDofsS,intDofsS));
+        Mat = kron(Mtt(1:end-1,2:end),Ks(intDofsS,intDofsS))-kron(Kt(1:end-1,2:end),Ms(intDofsS,intDofsS));
+        if ~isempty(rbnSides)
+            Mat = Mat+kron(Wt(1:end-1,2:end),Mr(intDofsS,intDofsS));
         end
-        solution.u(trialIntDofs) = Mat\rhs(intDofs2);
+        solution.u(trialIntDofs) = Mat\rhs(testIntDofs);
     case 'dirEff'
         [U,Ds,Q,Z,Tr,A] = FTparameters(Ks(intDofsS,intDofsS),Kt(1:end-1,2:end),Ms(intDofsS,intDofsS),Mtt(1:end-1,2:end));
         clear Ks Kt Ms Wt intDofsS;
-        solution.u(trialIntDofs) = FTapplication(rhs(intDofs2),U,Ds,Q,Z,Tr,A);
+        solution.u(trialIntDofs) = FTapplication(rhs(testIntDofs),U,Ds,Q,Z,Tr,A);
         clear U Ds Q Z Tr A;
     case 'precT'
         [U,Ds,Q,Z,Tr,A] = FTparameters(Ks(intDofsS,intDofsS),Kt(1:end-1,2:end),Ms(intDofsS,intDofsS),Mtt(1:end-1,2:end));
@@ -233,29 +243,39 @@ switch Solver
         if~exist('tol','var')
             tol=10^-12;
         end
-        [y,~,relres,iter] = gmres(Mat,rhs(intDofs2),50,tol,numel(intDofs1),Prec);
+        [y,~,relres,iter] = gmres(Mat,rhs(testIntDofs),50,tol,numel(trialIntDofs),Prec);
         solution.u(trialIntDofs) = y;
         fprintf(1,'\n*Solver info: iter=[%d,%d]; Relres=%g*\n',iter(1),iter(2),relres);
         clear intDofs2 U Ds Q Z Tr A Ks Kt Ms Mtt Mr Wt intDofsS iter relres;
 end
-solution.nDof = numel(intDofs1);
+solution.nDof = numel(trialIntDofs);
 clear intDofs1 intDofs2 rhs;
 
 %% Compute Errors
 if Error
-    [errH1,errL2,errH1s] = sp_h1_c_error_tp (solution.spaceST, solution.mshST, solution.u, u_ex, grad_u_ex, c);
-    [normH1,normL2,normH1s] = sp_h1_c_error_tp (solution.spaceST, solution.mshST, zeros(size(solution.u)), u_ex, grad_u_ex, c);
+    [errH1,errL2,errH1s] = sp_h1_c_error_tp (solution.spaceST, solution.mshST, solution.u, u_ex, grad_u_ex,c);
+    [normH1,normL2,normH1s] = sp_h1_c_error_tp (solution.spaceST, solution.mshST, zeros(size(solution.u)), u_ex, grad_u_ex,c);
     solution.relErrH1 = errH1/normH1;
     solution.relErrL2 = errL2/normL2;
     solution.relErrH1s = errH1s/normH1s;
-    clear errH1s errH1 errL2 normH1s normH1 normL2;
+end
+
+if ErrorFine
+    solFine=load(solFine).solution;
+    [errH1, errL2, errH1s] = sp_h1_error_spline_ex(solution.spaceST,solution.mshST,solution.u,solFine.spaceST,solution.geometryST,solFine.u,c);
+    [normH1,normL2,normH1s] = sp_h1_error_spline_ex(solution.spaceST,solution.mshST,solution.u*0,solFine.spaceST,solution.geometryST,solFine.u,c);
+    solution.relErrH1=errH1/normH1;
+    solution.relErrL2=errL2/normL2;
+    solution.relErrH1s=errH1s/normH1s;
 end
 
 %% Compute Energy at t=0 and t=T
 if Energy
     solution.E0Et = computeEnergy(solution.mshST.qn,solution.mshST.qw,solution.u,c,solution.spaceST,solution.geometryST,solution.mshST.map_der,[0,1]);
 end
+
 end
+
 
 function gDrchlt = InitDrchlt3d(gDrchlt,gInit,x,y,z,t,dimS,iside)
 if iside == 2*dimS+1
